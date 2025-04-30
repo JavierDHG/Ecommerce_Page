59b716647dc3196533febe844827cc55cec6dcb6
Devion <madara098javier@gmail.com>
2025-04-30 10:35:27 -0500

    Fixed_14

--- backend/django_ecommerce_api/settings.py
+++ backend/django_ecommerce_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
