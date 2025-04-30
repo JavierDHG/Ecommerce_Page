cb11ce275f616a729fd8735ff8e51e3a2053371b
Devion <madara098javier@gmail.com>
2025-04-30 13:25:10 -0500

    Fixed_25

---
+++ backend/entrypoint.sh
@@ -0,0 +1,8 @@
+# Esperar a la base de datos si es necesario
+/wait-for-db.sh  # Si tienes este script
+
+# Ejecutar migraciones
+python manage.py migrate
+
+# Ejecutar el comando principal (CMD)
+exec "$@"
