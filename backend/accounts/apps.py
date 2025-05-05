dc4ff3950bb536f419c1780690317b90fcb415c1
Devion <madara098javier@gmail.com>
2025-05-05 12:29:09 -0500

    Refactor AccountsConfig to include superuser creation logic in the ready method

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import logging
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+     def ready(self):
+        from django.db.utils import OperationalError
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'adminU@admin_us.com', 'persia098$')
+                logging.info('Superusuario creado.')
+            else:
+                logging.info('El superusuario ya existe.')
+        except OperationalError:
+            # Ocurre cuando la DB aún no está lista, como en migraciones iniciales
+            pass
