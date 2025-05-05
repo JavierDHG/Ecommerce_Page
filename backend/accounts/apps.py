9a002d1c8a148f90ff3dc7e7678525540853726b
Devion <madara098javier@gmail.com>
2025-05-05 12:34:47 -0500

    Refactor AccountsConfig to import OperationalError at the class level

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,13 +1,13 @@
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 import logging
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
-     def ready(self):
-        from django.db.utils import OperationalError
+    def ready(self):
         try:
             User = get_user_model()
             if not User.objects.filter(username='admin').exists():
