89e41dc06ee18f795fd00ea5939920aa0622b2e0
Devion <42222973+JavierDHG@users.noreply.github.com>
2025-08-12 12:10:23 -0500

    Update create_products.js

--- frontend/src/services/create_products.js
+++ frontend/src/services/create_products.js
@@ -5,7 +5,7 @@ const createProduct = async (product) => {
   console.log("Data received in createProduct:", product); // 👈 Verifica que el producto tenga la estructura correcta
   try {
     const response = await axios.post(
-      "https://ecostore-api.onrender.com/api/v1/products/",
+      "####",
       product, // 👈 Axios lo convierte automáticamente a JSON
       {
         headers: {
