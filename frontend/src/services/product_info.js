6db0ee0cbfeade5708d0b92e555332aaa091db56
Devion <42222973+JavierDHG@users.noreply.github.com>
2025-08-12 12:12:02 -0500

    Update product_info.js

--- frontend/src/services/product_info.js
+++ frontend/src/services/product_info.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProductDetails = async (productId) => {
   try {
     const response = await axios.get(
-      `https://ecostore-api.onrender.com/api/v1/products/${productId}/`
+      `####`
     );
     return response.data;
   } catch (error) {
